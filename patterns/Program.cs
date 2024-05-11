017d4ca449f75aed2aadc8e22ba2a879f434abaf
Fremy <dp8446732@gmail.com>
2024-05-11 15:28:29 +0200

    Добавьте файлы проекта.

---
+++ patterns/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+class Computer
+{
+    public void PowerOn()
+    {
+        Console.WriteLine("Компьютер включен");
+    }
+
+    public void PowerOff()
+    {
+        Console.WriteLine("Компьютер выключен");
+    }
+
+    public void BootOS()
+    {
+        Console.WriteLine("ОС загружается");
+    }
+
+    public void LaunchProgram(string programName)
+    {
+        Console.WriteLine($"Программа {programName} запущена");
+    }
+}
+
+class ComputerFacade
+{
+    private Computer computer;
+
+    public ComputerFacade()
+    {
+        computer = new Computer();
+    }
+
+    public void TurnOn()
+    {
+        computer.PowerOn();
+        computer.BootOS();
+    }
+
+    public void TurnOff()
+    {
+        computer.PowerOff();
+    }
+
+    public void StartProgram(string programName)
+    {
+        computer.PowerOn();
+        computer.BootOS();
+        computer.LaunchProgram(programName);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ComputerFacade facade = new ComputerFacade();
+
+        facade.TurnOn();
+
+        facade.StartProgram("Notepad");
+
+        facade.TurnOff();
+    }
+}
